a63a009d3ed9ba06eefadbba9f9c01c43d75a381
Kyle Cook <kcook@ucsd.edu>
2018-08-08 11:21:48 -0700

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20180807182949) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_diegst"
     t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
